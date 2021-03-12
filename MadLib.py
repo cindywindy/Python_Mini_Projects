3715e3ce847a7a8bacd26e096490f7f15ceec333
cindywindy <salc3657@gmail.com>
2021-03-12 09:08:29 -0700

    Different concatanation methods used to create MadLib

---
+++ MadLib.py
@@ -0,0 +1,11 @@
+#concatenating strings
+
+
+noun = input("Plural Noun: ")
+
+print("Machine learning can be applied to gain insights about " + noun + ".")
+print("{} may generate more data than one may initally think.".format(noun))
+#fstring
+print(f"But if you start by thinking of what you like about {noun}, you can branch off from there and think about how you could study that more deeply in quantitative ways.")
+madlib = f"I'm so excited to see what we will learn about {noun}! In no time, you will be an expert on {noun}!"
+print(madlib)
